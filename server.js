2865f73dbd5b70a226e3f3e1b998c27e98508a39
Kgomotso001 <53516725+Kgomotso001@users.noreply.github.com>
2020-07-04 22:06:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/icep'));
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+
-        './icep/dist/index.html'));});
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/icep/index.html'));});
 app.listen(process.env.PORT || 8080);
