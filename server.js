c3adbe6e4b5b3215c793e89896df03e62ea8ae2f
deehunnid <kgomotsodilebo@gmail.com>
2020-07-04 21:54:23 +0200

    fixing things really

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/icep'));
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+
-        '/dist/icep/index.html'));});
+        './icep/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
