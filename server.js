bfe1d258e401d2dab5894757cca4be648e634099
deehunnid <kgomotsodilebo@gmail.com>
2020-07-04 21:14:24 +0200

    commit after pull

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const app = express();
 app.us(express.static(_dirname + '/dist/icep'));
 app.get('/*', function(req,res){
     res.sendFile(path.join(_dirname+
+        '/dist/icep/index.html'));});
+app.listen(process.env.PORT || 8080);
+*/
+import express, { static } from 'express';
+import { join } from 'path';
+const app = express();
+app.us(static(_dirname + '/dist/icep'));
+app.get('/*', function(req,res){
+    res.sendFile(join(_dirname+
         '/dist/icep/index.html'));});
 app.listen(process.env.PORT || 8080);
